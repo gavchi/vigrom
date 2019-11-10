1c725b2f9129f2956336a98ecadc80cd2bc8205c
gavchi <gavchi@gmail.com>
2019-11-11 01:48:23 +0300

    refund для всех типов транзакций

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -6,15 +6,13 @@ use App\Models\Transaction;
 use Faker\Generator as Faker;
 
 $factory->define(Transaction::class, function (Faker $faker) {
-    $type = $faker->randomElement(['debit', 'credit', 'credit', 'credit']); //для большей вероятности + на балансе
-    $cause = 'debit' === $type ? 'refund' : $faker->randomElement(['stock', 'gift']);
     $date = $faker->dateTimeInInterval('-10 days');
 
     return [
         'currency_id' => $faker->numberBetween(1, 2),
         'amount' => round($faker->randomFloat(6, 0, 100000), 2),
-        'type' => $type,
-        'cause' => $cause,
+        'type' => $faker->randomElement(['debit', 'credit', 'credit', 'credit']), //для большей вероятности + на балансе
+        'cause' => $faker->randomElement(['stock', 'gift', 'refund']),
         'created_at' => $date,
         'updated_at' => $date,
     ];
